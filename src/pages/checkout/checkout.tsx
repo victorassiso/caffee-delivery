d441231ad451331d41bf12dd28d45db9c334cca5
Victor Assis <victor_assis@poli.ufrj.br>
2024-01-06 13:04:46 -0300

    ADD: Create Checkout Page

---
+++ src/pages/checkout/checkout.tsx
@@ -0,0 +1,7 @@
+export function Checkout() {
+  return (
+    <div className="mt-[104px] flex w-full flex-col items-center">
+      <h1>Checkout</h1>
+    </div>
+  )
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './layouts/app'
-import { Home } from './pages/home'
+import { Checkout } from './pages/checkout/checkout'
+import { Home } from './pages/home/home'
 
 export const router = createBrowserRouter(
   [
     {
       path: '/',
       element: <AppLayout />,
-      children: [{ path: '/', element: <Home /> }],
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/checkout', element: <Checkout /> },
+      ],
     },
   ],
   { basename: '/coffee-delivery' },
